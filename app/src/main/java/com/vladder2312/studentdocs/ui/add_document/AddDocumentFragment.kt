0d6216aafe3e06eb08b43ed05cd61a38a73f4e26
Vlad <vladder2312@mail.ru>
2020-06-01 21:22:08 +0300

    Создал layout фрагмента добавления документа

--- app/src/main/java/com/vladder2312/studentdocs/ui/add_document/AddDocumentFragment.kt
+++ app/src/main/java/com/vladder2312/studentdocs/ui/add_document/AddDocumentFragment.kt
@@ -1,16 +1,24 @@
 package com.vladder2312.studentdocs.ui.add_document
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
+import com.vladder2312.studentdocs.R
 
-class AddDocumentFragment : MvpAppCompatFragment() {
+class AddDocumentFragment : MvpAppCompatFragment(), AddDocumentView {
 
     @InjectPresenter
     lateinit var presenter: AddDocumentPresenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_document, container, false)
     }
 }
