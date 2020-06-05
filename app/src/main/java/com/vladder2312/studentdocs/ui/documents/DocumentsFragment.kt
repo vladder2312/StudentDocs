f1f40a506369e467ae476b3862d12f34a947e290
Vlad <vladder2312@mail.ru>
2020-06-06 01:00:58 +0300

    Поиск документа по названию

--- app/src/main/java/com/vladder2312/studentdocs/ui/documents/DocumentsFragment.kt
+++ app/src/main/java/com/vladder2312/studentdocs/ui/documents/DocumentsFragment.kt
@@ -2,14 +2,12 @@ package com.vladder2312.studentdocs.ui.documents
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.vladder2312.studentdocs.R
-import com.vladder2312.studentdocs.domain.Category
 import com.vladder2312.studentdocs.domain.Document
 import com.vladder2312.studentdocs.ui.document.DocumentActivity
 import kotlinx.android.synthetic.main.fragment_documents.*
@@ -19,6 +17,7 @@ class DocumentsFragment : MvpAppCompatFragment(), DocumentsView {
 
     @InjectPresenter
     lateinit var presenter: DocumentsPresenter
+    private lateinit var search: SearchView
     private val adapter = EasyAdapter()
     private val documentController = DocumentsController {
         startDocumentActivity(it)
@@ -31,6 +30,21 @@ class DocumentsFragment : MvpAppCompatFragment(), DocumentsView {
     ): View? {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_documents, container, false)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.documents_menu, menu)
+        search = menu.getItem(0).actionView as SearchView
+        search.setOnQueryTextListener(presenter.queryTextListener)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/vladder2312/studentdocs/ui/documents/DocumentsPresenter.kt
+++ app/src/main/java/com/vladder2312/studentdocs/ui/documents/DocumentsPresenter.kt
@@ -1,14 +1,13 @@
 package com.vladder2312.studentdocs.ui.documents
 
-import androidx.room.RxRoom
+import androidx.appcompat.widget.SearchView
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.vladder2312.studentdocs.App
 import com.vladder2312.studentdocs.data.repositories.DocRepository
-import com.vladder2312.studentdocs.domain.Document
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
+import java.util.*
 import javax.inject.Inject
 
 @InjectViewState
@@ -17,6 +16,20 @@ class DocumentsPresenter : MvpPresenter<DocumentsView>() {
     @Inject
     lateinit var docRepository: DocRepository
     val model = DocumentsModel()
+    val queryTextListener = object : SearchView.OnQueryTextListener {
+
+        override fun onQueryTextSubmit(query: String?) = true
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            if (newText != null) {
+                viewState.setData(model.documents.filter {
+                    it.name.toLowerCase(Locale.getDefault())
+                        .contains(newText.trim().toLowerCase(Locale.getDefault()))
+                })
+            }
+            return true
+        }
+    }
 
     init {
         App.appComponent.inject(this)
@@ -27,7 +40,8 @@ class DocumentsPresenter : MvpPresenter<DocumentsView>() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                viewState.setData(it)
+                model.documents.addAll(it)
+                viewState.setData(model.documents)
             }
     }
 }
